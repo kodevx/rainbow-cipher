cbe57843d6bd235fc8532613878612342c233692
Rohith Nair <rohithnair2910@gmail.com>
2026-04-22 16:57:50 +0530

    Initial Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <React.Fragment>
+      <div className={'text-4xl'}>
+        Hello world!
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default App
