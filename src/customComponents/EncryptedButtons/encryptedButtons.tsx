50c307338e4cb02dd88b41188f1b981aacd96caf
Rohith Nair <rohithnair2910@gmail.com>
2026-07-12 15:37:13 +0400

    feat: added encrypted buttons component

---
+++ src/customComponents/EncryptedButtons/encryptedButtons.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const EncryptedButtons = (props) => {
+
+    return (
+        <div>
+            encrypted buttons ...
+        </div>
+    )
+}
+
+
+export default EncryptedButtons
