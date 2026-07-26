4e1801af651a89618aa125391daf457c86b86621
Rohith Nair <rohithnair2910@gmail.com>
2026-07-26 12:28:43 +0400

    feat: add base path url in tanstack router

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import './main.css';
 import { routeTree } from "./routeTree.gen";
 
 // Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({ routeTree, basepath: "/rainbow-cipher", });
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
